49722b219be6b79a8b288289dbab855d8e4459fe
Robert Konradi <robert.konradi@gmx.net>
2018-04-19 13:00:39 +0200

    neu

---
+++ V602/content/plot1.py
@@ -0,0 +1,37 @@
+import matplotlib as mpl
+mpl.use('pgf')
+import numpy as np
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+mpl.rcParams.update({
+'font.family': 'serif',
+'text.usetex': True,
+'pgf.rcfonts': False,
+'pgf.texsystem': 'lualatex',
+'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
+})
+
+
+ y, x_1 = np.genfromtxt('content/data1.txt', unpack=True)
+
+plt.plot(x_1, y, r'bx', label=r'Messwerte')
+#plt.axvline(9.84, color='black', linestyle=':', label='Maximum')
+
+def f(x_1, m, b):
+    y = m*x_1+b
+    return y
+
+params, covariance_matrix = curve_fit(f, x_1, y)
+
+errors = np.sqrt(np.diag(covariance_matrix))
+
+plt.plot(x_1, f(x_1, *params), 'b-', label='Regression')
+plt.legend()
+plt.grid()
+plt.xlabel(r'$Z^2$')
+plt.ylabel(r'$\sqrt E / keV$')
+plt.tight_layout()
+plt.savefig('plot1.pdf')
+
+print('m=', params[0], '+-', errors[0])
+print('b=', params[1], '+-', errors[1])
