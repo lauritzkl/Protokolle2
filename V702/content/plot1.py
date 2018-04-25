3c9263ecbf4fb98fcb98657dbf31594fa182f301
Lauritz Klünder <lauritz.kluender@yahoo.de>
2018-04-25 13:59:26 +0200

    ss

--- V702/content/plot1.py
+++ V702/content/plot1.py
@@ -11,27 +11,17 @@ mpl.rcParams.update({
 'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
 })
 
+U = 2.5
+x, y_2 = np.genfromtxt('content/data1.txt', unpack=True)
 
-x, y_1 = np.genfromtxt('content/data1.txt', unpack=True)
-
+y_1 = (y_2-U)
 y = np.log(y_1)
 
-#def f(x, m, b):
-#    y = m*x+b
-#    return y
-
-#params, covariance_matrix = curve_fit(f, x, y)
-
-#errors = np.sqrt(np.diag(covariance_matrix))
 
 plt.plot(x, y, r'rx', label='Messwerte')
-#plt.plot(x, f(x, *params), 'k-', label='Regression')
 plt.legend()
 plt.grid()
-plt.ylabel(r'$N_{\Delta t}$')
+plt.ylabel(r'$\ln(N_{\Delta t})$')
 plt.xlabel(r'$t \, / \, \si{\second}$')
 plt.tight_layout()
 plt.savefig('plot1.pdf')
-
-#print('m=', params[0], '+-', errors[0])
-#print('b=', params[1], '+-', errors[1])
