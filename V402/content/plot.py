1359dc35a08949a4bd75efab1c581d5a64c2be45
Lauritz Klünder <lauritz.kluender@yahoo.de>
2018-06-07 17:03:05 +0200

    ss

--- V402/content/plot.py
+++ V402/content/plot.py
@@ -13,43 +13,40 @@ mpl.rcParams.update({
 'pgf.preamble': r'\usepackage{unicode-math}\usepackage{siunitx}',
 })
 
-x, y = np.genfromtxt('content/data1.txt', unpack=True)
+x, y_1 = np.genfromtxt('content/data1.txt', unpack=True)
+
+y = y_1**2
 
 def f(x, a, b):
-   y = np.sqrt(b+a/x**2)
+   y = a + b/(x**2)
    return y
 
 params, covariance_matrix = curve_fit(f, x, y)
 errors = np.sqrt(np.diag(covariance_matrix))
 
+def g(x, c, d):
+   y = c - d * x**2
+   return y
+
+params1, covariance_matrix1 = curve_fit(g, x, y)
+errors1 = np.sqrt(np.diag(covariance_matrix1))
+
+x_plot = np.linspace(min(x), max(x), 1000)
+
 plt.plot(x, y, r'rx', label='Messwerte')
-plt.plot(x, f(x, *params), 'k-', label='Regression')
+plt.plot(x_plot, f(x_plot, *params), 'k-', label='Regression für $\lambda >>\lambda_1$')
+plt.plot(x_plot, g(x_plot, *params1), 'k-', label='Regression für $\lambda << \lambda_1$')
 plt.legend()
 plt.grid()
-plt.ylabel(r'$n$')
-plt.xlabel(r'$Wellenlänge \, / \, nm$')
+plt.ylabel(r'$n^2$')
+plt.xlabel(r'$\lambda \, / \, nm$')
 plt.savefig('plot1.pdf')
 plt.close()
 print('a=', params[0], '+-', errors[0])
 print('b=', params[1], '+-', errors[1])
+print('c=', params1[0], '+-', errors1[0])
+print('d=', params1[1], '+-', errors1[1])
 
-
-def g(x, c, d):
-   y = np.sqrt(c-d*x**2)
-   return y
-
-params, covariance_matrix1 = curve_fit(g, x, y)
-errors = np.sqrt(np.diag(covariance_matrix1))
-plt.plot(x, y, r'rx', label='Messwerte')
-plt.plot(x, f(x, *params), 'k-', label='Regression')
-plt.legend()
-plt.grid()
-plt.ylabel(r'$n$')
-plt.xlabel(r'$Wellenlänge \, / \, nm$')
-plt.savefig('plot2.pdf')
-plt.close()
-print('c=', params[0], '+-', errors[0])
-print('d=', params[1], '+-', errors[1])
 
 #Berechnung der Abweichungsquadrate
 
