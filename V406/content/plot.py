16c9c8f60922c19cb83da6631f5a336b32b7d308
Lauritz Klünder <lauritz.kluender@yahoo.de>
2018-06-23 14:16:27 +0200

    ss

--- V406/content/plot.py
+++ V406/content/plot.py
@@ -14,101 +14,102 @@ mpl.rcParams.update({
 })
 
 #Messung am Spalt 1
-z, w = np.genfromtxt('content/data.txt', unpack=True)
+z, y = np.genfromtxt('content/data.txt', unpack=True)
 
 x= 23.75 - z  #Abzug vom Hauptmaxima
-y= w - 0.0093 #Abzug vom Dunkelstrom
+#y= w - 0.0093 #Abzug vom Dunkelstrom
 
 p = x / 1000 #Winkel phi = x /L
-I= y * 1000
+I= y
 l= 635 * 10**(-9)
 
-x_l = x[1:30]
-y_l = y[1:30]
-x_r = x[30:]
-y_r = y[30:]
 
-def f(x_l, a, b, c, d):
-   y = a * ((l*b/(np.pi * np.sin(x_l - d)))**2) * (np.sin((np.pi * b * np.sin(x_l - d))/l)**2) + c
+def f(p, a, b, c, d):
+   y = a * ((l/(np.pi * np.sin(p - d)))**2) * (np.sin((np.pi * b * np.sin(p - d))/l)**2) + c
    return y
 
-params, covariance_matrix = curve_fit(f, p, y)
+params, covariance_matrix = curve_fit(f, p, y, p0=((10**10), (0.15*10**(-3)), 1, (-0.0002)))
 errors = np.sqrt(np.diag(covariance_matrix))
 t = np.linspace(min(p), max(p), 1000)
 
 plt.plot(p, I, r'rx', label='Messwerte')
-plt.plot(t, f(x_l, *params), 'k-', label='Regression')
+plt.plot(t, f(t, *params), 'k-', label='Regression')
 plt.legend()
 plt.grid()
-plt.ylabel('Strom proportional zur Intensität / nA')
+plt.ylabel('Strom proportional zur Intensität / ')
 plt.xlabel(r'$\varphi$ / rad')
 plt.savefig('plot1.pdf')
 plt.close()
 
-#print('a =', params[0], '+/-', err[0])
-#print('b =', params[1], '+/-', err[1])
-#print('c =', params[2], '+/-', err[2])
-#print('d =', params[3], '+/-', err[3])
+print('Spalt 0,15mm:')
+print('a =', params[0], '+/-', errors[0])
+print('b =', params[1], '+/-', errors[1])
+print('c =', params[2], '+/-', errors[2])
+print('d =', params[3], '+/-', errors[3])
 
 
 #Messung am Spalt 2
 
-k, l = np.genfromtxt('content/data2.txt', unpack=True)
+k, y = np.genfromtxt('content/data2.txt', unpack=True)
 x= (24.3 - k)
-y= (l - 0.0093)*10**(-6)
+l= 635 * 10**(-9)
+
 
 p = x / 1000 #Winkel phi
 
-#def f(x, a, b, c, d):
-#   y = a * (((635 * 10**(-9))/(np.pi * np.sin(x - d)))**2) * ((np.sin((np.pi * b * np.sin(x - d))/(635 * 10**(-9))))**2) + c
-#   return y
-#
-#params, covariance_matrix = curve_fit(f, x, y)
-#errors = np.sqrt(np.diag(covariance_matrix))
-#t = np.linspace(min(x), max(x), 1000)
+def f(p, a, b, c, d):
+   y = a * ((l/(np.pi * np.sin(p - d)))**2) * (np.sin((np.pi * b * np.sin(p - d))/l)**2) + c
+   return y
+
+params, covariance_matrix = curve_fit(f, p, y, p0=((10**10), (0.075*10**(-3)), 1, (-0.0002)))
+errors = np.sqrt(np.diag(covariance_matrix))
+t = np.linspace(min(p), max(p), 1000)
 
 plt.plot(p, y, r'rx', label='Messwerte')
-#plt.plot(t, f(x, *params), 'k-', label='Regression')
+plt.plot(t, f(t, *params), 'k-', label='Regression')
 plt.legend()
 plt.grid()
 plt.ylabel('Strom proportional zur Intensität / nA')
 plt.xlabel(r'$\varphi$ / rad')
 plt.savefig('plot2.pdf')
 plt.close()
-#print('a =', params[0], '+/-', err[0])
-#print('b =', params[1], '+/-', err[1])
-#print('c =', params[2], '+/-', err[2])
-#print('d =', params[3], '+/-', err[3])
+
+print('Spalt 0,075mm:')
+print('a =', params[0], '+/-', errors[0])
+print('b =', params[1], '+/-', errors[1])
+print('c =', params[2], '+/-', errors[2])
+print('d =', params[3], '+/-', errors[3])
 
 
 
 #Messung am Doppelspalt
 
-k, l = np.genfromtxt('content/data3.txt', unpack=True)
+k, y = np.genfromtxt('content/data3.txt', unpack=True)
 x= (24.2 - k)
-y= (l - 0.0093)*10**(-6)
+l= 635 * 10**(-9)
 
 p = x / 1000 #Winkel phi
 
-#def f(x, a, s, b, d):
-#  a * ((np.cos((np.pi * s * np.sin(x - d))/(635 * 10**(-9))))**2) * (((635 * 10**(-9))/(np.pi * b * np.sin(x - d)))**2) * ((np.sin((np.pi * b * np.sin(x - d))/(635 * 10**(-9))))**2)
-#   return y
-#
-#params, covariance_matrix = curve_fit(f, x, y)
-#errors = np.sqrt(np.diag(covariance_matrix))
-#t = np.linspace(min(x), max(x), 1000)
+def f(p, a, s, b, d):
+  y = a * ((np.cos((np.pi * s * np.sin(p - d))/l))**2) * ((l/(np.pi * b * np.sin(p - d)))**2) * ((np.sin((np.pi * b * np.sin(p - d))/l))**2)
+  return y
+
+params, covariance_matrix = curve_fit(f, p, y, p0=((6*10**3), (0.5*10**(-3)), (0.15*10**(-3)), (- 0.0002)))
+errors = np.sqrt(np.diag(covariance_matrix))
+t = np.linspace(min(p), max(p), 1000)
 
 plt.plot(p, y, r'rx', label='Messwerte')
-#plt.plot(t, f(x, *params), 'k-', label='Regression')
+plt.plot(t, f(t, *params), 'k-', label='Regression')
 plt.legend()
 plt.grid()
 plt.ylabel('Strom proportional zur Intensität / nA')
 plt.xlabel(r'$\varphi$ / rad')
 plt.savefig('plot3.pdf')
 plt.close()
-#print('a =', params[0], '+/-', err[0])
-#print('b =', params[1], '+/-', err[1])
-#print('c =', params[2], '+/-', err[2])
-#print('d =', params[3], '+/-', err[3])
+print('Doppelspalt:')
+print('a =', params[0], '+/-', errors[0])
+print('s =', params[1], '+/-', errors[1])
+print('b =', params[2], '+/-', errors[2])
+print('d =', params[3], '+/-', errors[3])
 
 #Messung Vergleichen
