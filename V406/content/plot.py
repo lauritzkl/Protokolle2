70a9ffa7bf8eb8c7b791b8f6e6ca93e4a15bf594
Lauritz Klünder <lauritz.kluender@yahoo.de>
2018-06-24 19:01:21 +0200

    ss

--- V406/content/plot.py
+++ V406/content/plot.py
@@ -114,41 +114,34 @@ print('d =', params[3], '+/-', errors[3])
 
 #Messung Vergleichen
 
-x, I = np.genfromtxt('content/data3.txt', unpack=True)
+k, y = np.genfromtxt('content/data3.txt', unpack=True)
+x= (24.2 - k)
+l= 635 * 10**(-9)
+p = x / 1000 #Winkel phi
 
-p = x / 1000     # x / L
-l = 635 * 10**(-9)
-shiftdop = -0.000180171874364
-shiftp = p - shiftdop
+np.savetxt('content/Winkel3.txt', np.column_stack([p]), header="Der Winkel von Messung 3")
 
-plt.plot(shiftp, I, 'kx', label='Messwerte Doppelspalt')
+def f(p, a, s, b, d):
+  y = a * ((np.cos((np.pi * s * np.sin(p - d))/l))**2) * ((l/(np.pi * b * np.sin(p - d)))**2) * ((np.sin((np.pi * b * np.sin(p - d))/l))**2)
+  return y
 
-A0dop = 1.6775075197717908
-sdop = 0.0005084773951663262
-bdop = 0.0001633336719143504
+params, covariance_matrix = curve_fit(f, p, y, p0=((6*10**3), (0.5*10**(-3)), (0.15*10**(-3)), (- 0.0002)))
+errors = np.sqrt(np.diag(covariance_matrix))
 
-A0sing = 20106583.17271117
-bsing = 0.00014944404494694484
-csing = 0.006199038950450398
+def d(p):
+    return 6.3e7 * ((l/(np.pi * np.sin(p - params[3])))**2) * (np.sin((np.pi * params[2] * np.sin(p - params[3]))/l)**2)
 
-def d(x, a, s, b):
-    y = a * (np.cos((np.pi * s * np.sin(x))/l)**2) * ((l/(np.pi * b * np.sin(x)))**2) * (np.sin((np.pi * b * np.sin(x))/l)**2)
-    return y
-
-def e(x, a, b, c):
-    y = a * ((l/(np.pi * np.sin(x)))**2) * (np.sin((np.pi * b * np.sin(x))/l)**2) + c
-    return y
 
 t = np.linspace(min(p), max(p), 1000)
-plt.plot(t, d(t, A0dop, sdop, bdop), 'b-', label='Doppelspalt')
-plt.plot(t, e(t, A0sing, bsing, csing), 'r-', label='Einzelspalt')
-plt.xlabel(r'$\varphi$ / rad')
-plt.ylabel('Strom proportional zur Intensität / nA')
-plt.legend(loc='best')
+
+plt.plot(p, y, r'rx', label='Messwerte')
+plt.plot(t, f(t, *params), 'k-', label='Doppelspalt')
+plt.plot(t, d(t), 'b-', label='Einzelspalt')
+plt.legend()
 plt.grid()
-plt.tight_layout()
+plt.ylabel('Strom proportional zur Intensität / µA')
+plt.xlabel(r'$\varphi$ / rad')
 plt.savefig('plot4.pdf')
-plt.show()
+plt.close()
 
-
-print(d((0.000001 / 1000), A0dop, sdop, bdop))
+#print(params1[0])
